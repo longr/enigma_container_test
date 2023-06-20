cb5f8631e4e9327ec76070a17a095701ce8cf97b
Robin Long <r.long@lancaster.ac.uk>
2023-06-20 11:15:24 +0100

    update and overhaul of analysis_script

--- analysis_script.sh
+++ analysis_script.sh
@@ -1,5 +1,5 @@
-# #!/bin/bash --login
-
+#!/bin/bash
+## May need to add subjid to output log.
 
 # FSL Setup
 FSLDIR=/usr/local/fsl
@@ -8,77 +8,70 @@ export FSLDIR PATH
 . ${FSLDIR}/etc/fslconf/fsl.sh
 
 
-
-# #$ -cwd
-# #$ -N SingularityRunPGSCVriend_WMLsegs_in_mni
-# #$ -pe smp.pe 32
-# #$ -t 1-3
-
-# Dr Hamied Haroon
-# hamied.haroon@manchester.ac.uk
-# 15th March 2023
-
-# ALREADY IN CONTAINER NOW
-## load FSL module
-#module load apps/binapps/fsl/6.0.5
-
-### these are Robin's notes - may need checking
-
 # assign paths for code and input data directories
-
-### Dir contains image of the docker container image that is built in build script
-export code_dir=/mnt/bmh01-rds/Hamied_Haroon_doc/ENIGMA-VascPD/UNets-pgs
-
-### parent dir for project and input data
-export data_path=/scratch/${USER}/ENIGMA-PD-Vasc_manual_seg/Sarah_WML_man_seg/Controls+PD
-
-echo code_dir  : ${code_dir}
-echo data_path : ${data_path}
-
+#export code_dir=/mnt/bmh01-rds/Hamied_Haroon_doc/ENIGMA-VascPD/UNets-pgs
+export code_dir=/code
+#export data_path=/scratch/${USER}/ENIGMA-PD-Vasc_manual_seg/Sarah_WML_man_seg/Controls+PD
+export data_path=/data
+echo code_dir  : ${code_dir} >> ${code_dir}/logs.txt 2>&1
+echo data_path : ${data_path} >> ${code_dir}/logs.txt 2>&1
+echo   >> ${code_dir}/logs.txt 2>&1
 
 # assign path and filename of the list of subject IDs saved as a text file
 export subjids_list=${data_path}/subjects.txt
-echo subjids_list : ${subjids_list}
+echo subjids_list : ${subjids_list} >> ${code_dir}/logs.txt 2>&1
 
 # read one line from the list of subject IDs
-export subjid=`awk "NR==$SGE_TASK_ID" ${subjids_list}`
-echo subjid : ${subjid}
-echo  
+# Get the line in the file subkids_list whose line
+# number is equal to the SGE_TASK_ID#
+#export subjid=`awk "NR==$SGE_TASK_ID" ${subjids_list}`
+
+# replace with 1 to test
+export subjid=`awk "NR==1" ${subjids_list}`
+echo subjid : ${subjid} >> ${code_dir}/logs.txt 2>&1
+echo   >> ${code_dir}/logs.txt 2>&1
 
 # search full paths and filenames for input T1 and FLAIR images in compressed NIfTI format
 export t1_fn=`find ${data_path}/${subjid}/niftis/*[Tt]1*.nii.gz`
 export flair_fn=`find ${data_path}/${subjid}/niftis/*[Ff][Ll][Aa][Ii][Rr]*.nii.gz`
-echo t1_fn    : ${t1_fn}
-echo flair_fn : ${flair_fn}
-echo  
+echo t1_fn    : ${t1_fn} >> ${code_dir}/logs.txt 2>&1
+echo flair_fn : ${flair_fn} >> ${code_dir}/logs.txt 2>&1
+echo   >> ${code_dir}/logs.txt 2>&1
 
 # assign path for output data directory and create it (if it doesn't exist)
 export data_outpath=${data_path}/UNet-pgs/${subjid}
 mkdir -p ${data_outpath}
-echo data_outpath : ${data_outpath}
+echo data_outpath : ${data_outpath}  >> ${code_dir}/logs.txt 2>&1
 
+# REL # Why under code dir?
 # assign path for a temporary data directory under the code directory and create it
 export temp_dir=${code_dir}/Controls+PD/${subjid}
 mkdir -p ${temp_dir}
-echo temp_dir     : ${temp_dir}
-echo  
+echo temp_dir     : ${temp_dir}  >> ${code_dir}/logs.txt 2>&1
+echo   >> ${code_dir}/logs.txt 2>&1
 
 # change into temporary data directory and create input and output subdirectories
+# directories are required by flair
+cd ${temp_dir}
 mkdir -p ${temp_dir}/input
 mkdir -p ${temp_dir}/output
 
 # change into input directory ${temp_dir}/input
+# flirt expects to be ran in the same fir (maybe able to do this
+# outside of dir, but paths would be long)
 cd ${temp_dir}/input
 
 # copy input T1 and FLAIR images here, renaming them
+## files need to be renamed otherwise overwritten when fslroi is called.
+## also need to keep orginal file for flirt command
 cp ${t1_fn}    t1vol_orig.nii.gz
 cp ${flair_fn} flairvol_orig.nii.gz
 
 # run FSL's fsl_anat tool on the input T1 image, with outputs saved to a new subdirectory ${temp_dir}/input/t1-mni.anat
-echo running fsl_anat on t1 in ${temp_dir}/input/t1-mni.anat/
+echo running fsl_anat on t1 in ${temp_dir}/input/t1-mni.anat/  >> ${code_dir}/logs.txt 2>&1
 fsl_anat -o t1-mni -i ./t1vol_orig.nii.gz
-echo fsl_anat done
-echo  
+echo fsl_anat done  >> ${code_dir}/logs.txt 2>&1
+echo   >> ${code_dir}/logs.txt 2>&1
 # fsl_anat -o t1-mni -i ./t1vol_orig.nii.gz --nocrop
 
 # create new subdirectory to pre-process input FLAIR image, change into it ${temp_dir}/input/flair-bet
@@ -86,7 +79,7 @@ mkdir ${temp_dir}/input/flair-bet
 cd ${temp_dir}/input/flair-bet
 
 # run FSL's tools on input FLAIR image to ensure mni orientation followed by brain extraction
-echo preparing flair in ${temp_dir}/input/flair-bet/
+echo preparing flair in ${temp_dir}/input/flair-bet/  >> ${code_dir}/logs.txt 2>&1
 fslreorient2std -m flair_orig2std.mat ../flairvol_orig.nii.gz flairvol
 bet flairvol.nii.gz flairvol_brain -m -R -S -B -Z -v
 
@@ -103,8 +96,8 @@ flirt -in flairvol.nii.gz -applyxfm -init flairbrain2t1brain.mat \
 
 # run FSL's convert_xfm to invert FLAIR to T1 transformation matrix
 convert_xfm -omat flairbrain2t1brain_inv.mat -inverse flairbrain2t1brain.mat
-echo flair prep done
-echo  
+echo flair prep done  >> ${code_dir}/logs.txt 2>&1
+echo    >> ${code_dir}/logs.txt 2>&1
 
 # change one directory up to ${temp_dir}/input
 cd ${temp_dir}/input
@@ -128,30 +121,29 @@ flirt -in T1.nii.gz -omat T1_croppedmore2roi.mat \
 cd ${temp_dir}
 
 # run UNets-pgs in Singularity
-echo running UNets-pgs Singularity in ${temp_dir}
+echo running UNets-pgs Singularity in ${temp_dir}  >> ${code_dir}/logs.txt 2>&1
 #singularity exec --cleanenv ${code_dir}/pgs_cvriend.sif sh /WMHs_segmentation_PGS.sh T1.nii.gz FLAIR.nii.gz results.nii.gz ./input ./output
+#Container contains code naturally now, no need to build.
 
-### Built into contianer, no need to run separate one.
+/WMHs_segmentation_PGS.sh T1.nii.gz FLAIR.nii.gz results.nii.gz ./input ./output >> ${code_dir}/logs.txt 2>&1
 
-./WMHs_segmentation_PGS.sh T1.nii.gz FLAIR.nii.gz results.nii.gz ./input ./output
-
-echo UNets-pgs done!
-echo  
+echo UNets-pgs done!  >> ${code_dir}/logs.txt 2>&1
+echo   >> ${code_dir}/logs.txt 2>&1
 
 # change into output directory ${temp_dir}/output
 cd ${temp_dir}/output
 
-echo processing outputs in ${temp_dir}/output/
+echo processing outputs in ${temp_dir}/output/  >> ${code_dir}/logs.txt 2>&1
 
 # copy required images and transformation/warp coefficients from ${temp_dir}/input here, renaming T1 and FLAIR
-cp ../input/T1_croppedmore2roi.mat                     .
-cp ../input/t1-mni.anat/T1.nii.gz                      T1_roi.nii.gz
-cp ../input/t1-mni.anat/T1_fullfov.nii.gz              .
-cp ../input/t1-mni.anat/T1_to_MNI_lin.mat              .
-cp ../input/t1-mni.anat/T1_to_MNI_nonlin_coeff.nii.gz  .
-cp ../input/t1-mni.anat/T1_roi2nonroi.mat              .
-cp ../input/flair-bet/flairbrain2t1brain_inv.mat       .
-cp ../input/flair-bet/flairvol.nii.gz                  FLAIR_orig.nii.gz
+cp ${temp_dir}/input/T1_croppedmore2roi.mat                     .
+cp ${temp_dir}/input/t1-mni.anat/T1.nii.gz                      T1_roi.nii.gz
+cp ${temp_dir}/input/t1-mni.anat/T1_fullfov.nii.gz              .
+cp ${temp_dir}/input/t1-mni.anat/T1_to_MNI_lin.mat              .
+cp ${temp_dir}/input/t1-mni.anat/T1_to_MNI_nonlin_coeff.nii.gz  .
+cp ${temp_dir}/input/t1-mni.anat/T1_roi2nonroi.mat              .
+cp ${temp_dir}/input/flair-bet/flairbrain2t1brain_inv.mat       .
+cp ${temp_dir}/input/flair-bet/flairvol.nii.gz                  FLAIR_orig.nii.gz
 
 # copy MNI T1 template images here
 cp ${FSLDIR}/data/standard/MNI152_T1_2mm.nii.gz        .
@@ -184,15 +176,15 @@ applywarp --in=results2t1roi.nii.gz --warp=T1_to_MNI_nonlin_coeff.nii.gz \
    --interp=nn --ref=${FSLDIR}/data/standard/MNI152_T1_2mm_brain.nii.gz
 
 # copy all contents of temporary data directory to output data directory, and delete temporary data directory
-echo copying all contents 
-echo  from ${temp_dir} 
-echo  to ${data_outpath}
-cp -r ${temp_dir}/* ${data_outpath}
-# echo deleting ${temp_dir}
+echo copying all contents   >> ${code_dir}/logs.txt 2>&1
+echo  from ${temp_dir}   >> ${code_dir}/logs.txt 2>&1
+echo  to ${data_outpath}  >> ${code_dir}/logs.txt 2>&1
+cp -r ${temp_dir}/* ${data_outpath}  >> ${code_dir}/logs.txt 2>&1
+# echo deleting ${temp_dir} 
 # rm -r ${temp_dir}
 
-echo all done!
-echo  
+echo all done!  >> ${code_dir}/logs.txt 2>&1
+echo    >> ${code_dir}/logs.txt 2>&1
 
 # change to ${data_outpath}
 cd ${data_outpath}
